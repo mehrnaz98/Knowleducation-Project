d255bc154e6913e6762dc7aceb19003931dc7ee5
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-22 22:24:48 +0330

    Add template structure for BaseAnimatedEye component
    It features a circular eye structure with a green border and a dark pupil; Mouse event listeners to move the pupil when hovering and reset its position on mouse leave; Responsive layout using Tailwind CSS classes for styling.
    The pupil's position will be animated based on mouse movements within the eye area.

---
+++ src/components/ui/BaseAnimatedEye.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="relative w-64 h-64 border-[50px] border-green-500 rounded-full flex items-center justify-center cursor-pointer"
+    @mousemove="movePupil"
+    @mouseleave="resetPupil"
+  >
+    <div
+      class="absolute w-16 h-16 bg-black rounded-full transition-transform"
+      :style="{ transform: `translate(${pupilX}px, ${pupilY}px)` }"
+    >
+      <div class="absolute w-4 h-4 bg-white rounded-full top-4 left-9"></div>
+    </div>
+  </div>
+</template>

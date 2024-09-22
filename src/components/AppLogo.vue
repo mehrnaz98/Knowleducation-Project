1c250d4c733572572be7b27a1bdf893dd62e3735
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-22 15:09:58 +0330

    Update logo left and right padding

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex justify-start px-5 space-x-2 text-xl">
+  <section class="flex justify-start px-7 space-x-2 text-xl">
     <div>👁</div>
     <div class="font-bold">Knowleducation</div>
   </section>

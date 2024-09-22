ad7f75f7115f1211f40fa7ce467aceee79f09bc2
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-22 20:45:02 +0330

    Update gradient line

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,7 +1,17 @@
 <template>
   <section class="bg-gray-200 text-gray-600 flex justify-center">
     <section class="py-7 mx-32">
-      <h1 class="text-7xl font-bold">Learn EDGE Cases</h1>
+      <h1 class="text-7xl font-bold mb-11">
+        Learn
+        <span class="relative inline-block">
+          EDGE
+          <div
+            class="absolute left-[-23px] right-0 bottom-[-10px] h-28 mb-[-60px] bg-gradient-to-r from-red-400 via-yellow-400 to-emerald-400 w-[130%] [clip-path:polygon(0_45%,_100%_41%,_100%_56%,_0_52%)] rounded-full"
+          ></div>
+        </span>
+        Cases
+      </h1>
+
       <p class="text-lg mt-10 leading-10">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, officiis
         doloribus excepturi dolore officia aliquid error ipsum eos ea quam

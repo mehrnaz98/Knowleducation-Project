419376798f6acdf6ea467db3c549786004cbb8f2
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-22 14:41:38 +0330

    Update AppHeader.vue
    Updated styles for the navbar and button. Divided the header into two sections, one for AppLogo and one for the nav bar.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,25 @@
 <template>
-  <header class="flex justify-between items-center p-4">
-    <AppLogo />
-    <nav>
-      <ul class="flex space-x-4">
-        <li><a href="#">Courses</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li><a href="#">Lorem</a></li>
-        <button>Contact Us</button>
-      </ul>
-    </nav>
+  <header class="bg-gray-100 p-5">
+    <div
+      class="bg-white flex items-center border-4 border-gray-600 rounded-full w-"
+    >
+      <section class="w-1/4">
+        <AppLogo />
+      </section>
+
+      <section class="w-3/4">
+        <nav>
+          <ul class="flex space-x-7 justify-end items-center px-10">
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Lorem</a></li>
+            <button class="bg-green-500 text-white rounded-full px-9 py-2">
+              Contact Us
+            </button>
+          </ul>
+        </nav>
+      </section>
+    </div>
   </header>
 </template>
 

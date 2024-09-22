b2414ddc6120a7f05df304518d3a37a4a697aed1
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-22 22:32:49 +0330

    Initialize Reactive Variables
    This step sets up reactive variables for the pupil's current position (pupilX, pupilY) and the target position (targetX, targetY). It also declares a variable (animationFrameId) to manage the animation frame ID for smooth pupil movement.

--- src/components/ui/BaseAnimatedEye.vue
+++ src/components/ui/BaseAnimatedEye.vue
@@ -12,3 +12,13 @@
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const pupilX = ref<number>(0);
+const pupilY = ref<number>(0);
+let targetX = 0;
+let targetY = 0;
+let animationFrameId: number | null = null;
+</script>

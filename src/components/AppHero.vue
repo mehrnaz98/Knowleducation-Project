f35c603ccd46bf2b558b5003fb2cec629771b05e
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-22 15:25:15 +0330

    Update AppHero.vue

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,7 +1,20 @@
 <template>
-  <section class="flex justify-around">
+  <section class="flex justify-center">
     <section class="">
       <h1 class="">Learn EDGE Cases</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, officiis
+        doloribus excepturi dolore officia aliquid error ipsum eos ea quam
+        molestias quisquam natus, corrupti iusto suscipit tempore a. Mollitia
+        recusandae doloremque ut, fugit magni pariatur ipsum? Saepe, illo eaque
+        dignissimos soluta molestias sapiente nemo? Harum voluptates aspernatur
+        consequatur beatae perspiciatis, eos incidunt odio, et quae aliquid quis
+        officia dignissimos aut voluptatem molestias recusandae eveniet iure
+        voluptate porro modi vitae dolores laborum, enim quas! Esse,
+        necessitatibus vitae? Laudantium, quis? Dignissimos eius repellendus,
+        ipsa iure vitae assumenda, quisquam, praesentium earum facilis obcaecati
+        odit?
+      </p>
     </section>
 
     <section>👁</section>

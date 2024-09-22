ce4b15517edb45d1604e99912a4b9d64cc57248d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-22 15:16:07 +0330

    Import AppHero.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <AppHeader />
+  <AppHero />
 </template>
 
 <script setup lang="ts">
 import AppHeader from "./components/AppHeader.vue";
+import AppHero from "./components/AppHero.vue";
 </script>

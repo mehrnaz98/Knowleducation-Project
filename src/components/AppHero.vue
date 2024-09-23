50084169d320bb22574f876da064e461b2588509
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 12:35:48 +0330

    Update AppHero.vue

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -23,13 +23,13 @@
       <div class="relative flex justify-center">
         <input
           type="search"
-          placeholder="What do you want to learn"
+          placeholder="What do you want to learn?"
           class="bg-white shadow-sm w-[100%] border rounded-full mr-52 mt-5 mb-10 py-4 pl-5 pr-24"
         />
         <button
           class="absolute w-[15%] bg-green-500 text-white right-[29%] top-[41.5%] transform -translate-y-1/2 h-10 rounded-full text-base font-normal px-6"
         >
-          Search
+          search
         </button>
       </div>
     </section>

4cad887cffbf675b08246b53bb42d9f19d02dcda
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 13:14:18 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,5 @@
 <script setup lang="ts">
 import AppHeader from "./components/AppHeader.vue";
 import AppHero from "./components/AppHero.vue";
-import AppReasons from "./components/AppReasons.vue";
+import AppReasons from "./components/reasons/AppReasons.vue";
 </script>

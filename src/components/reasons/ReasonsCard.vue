7bc2f7ac520234369efb9133fe5baca17563d13f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 14:16:43 +0330

    Add display flex to the divs to place the image and texts side by side

--- src/components/reasons/ReasonsCard.vue
+++ src/components/reasons/ReasonsCard.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="w-96 border rounded-lg p-4 shadow-md">
-    <img
-      :src="image"
-      alt="Card Image"
-      class="w-full h-32 object-cover rounded-t-lg"
-    />
-    <h3 class="text-xl font-bold mt-2">{{ title }}</h3>
-    <p class="text-gray-600">{{ description }}</p>
+  <div class="flex w-96 border rounded-lg p-4 shadow-md">
+    <img :src="image" alt="Card Image" class="h-32 object-cover rounded-t-lg" />
+    <div>
+      <h3 class="text-xl font-bold mt-2">{{ title }}</h3>
+      <p class="text-gray-600">{{ description }}</p>
+    </div>
   </div>
 </template>
 

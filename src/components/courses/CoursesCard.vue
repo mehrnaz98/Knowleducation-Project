3356d9d908e15ca57b49af32a3436fc79e4c6572
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 22:39:52 +0330

    Display images if there are images, else, display the default div

--- src/components/courses/CoursesCard.vue
+++ src/components/courses/CoursesCard.vue
@@ -3,6 +3,17 @@
     class="relative bg-gray-400 flex flex-col w-[25.9rem] h-[33rem] rounded-2xl"
   >
     <div
+      v-if="image"
+      class="absolute bg-white w-[23rem] h-[23rem] top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
+    >
+      <img
+        :src="image"
+        alt="Course Image"
+        class="rounded-2xl w-full h-full object-cover"
+      />
+    </div>
+    <div
+      v-else
       class="absolute bg-white w-[23rem] h-[23rem] top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
     >
       <p class="uppercase text-6xl font-bold text-gray-400">image</p>

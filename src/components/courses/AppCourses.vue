5a7d8fd04fab171debe9ccf09007b74ec9002b8d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 22:32:41 +0330

    Import computed from vue

--- src/components/courses/AppCourses.vue
+++ src/components/courses/AppCourses.vue
@@ -12,6 +12,7 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { useCoursesStore } from "@/stores/useCoursesStore";
 import CoursesCard from "./CoursesCard.vue";
 

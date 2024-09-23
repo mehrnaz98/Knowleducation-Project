d9004b8641bc7c8c4f6ff6e6dae1066126907b68
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 10:31:53 +0330

    Bind Dynamic Pupil Size
    This step dynamically sets the width and height of the pupil using the pupilSize variable. The size of the pupil is now flexible and can be updated in response to changes in the pupilSize value.

--- src/components/ui/BaseAnimatedEye.vue
+++ src/components/ui/BaseAnimatedEye.vue
@@ -10,8 +10,12 @@
     @mouseleave="resetPupil"
   >
     <div
-      class="absolute w-16 h-16 bg-black rounded-full transition-transform"
-      :style="{ transform: `translate(${pupilX}px, ${pupilY}px)` }"
+      class="absolute bg-black rounded-full transition-transform"
+      :style="{
+        width: pupilSize + 'px',
+        height: pupilSize + 'px',
+        transform: `translate(${pupilX}px, ${pupilY}px)`,
+      }"
     >
       <div class="absolute w-4 h-4 bg-white rounded-full top-4 left-9"></div>
     </div>

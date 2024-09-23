e1a491f77f3daaf8b4ac2c00d34af6c4bb5bbb19
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 14:19:02 +0330

    Update AppReasons.vue
    Add info statically to check the layout

--- src/components/reasons/AppReasons.vue
+++ src/components/reasons/AppReasons.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="flex">
     <section class="w-2/5">
-      <ReasonsCard />
+      <ReasonsCard
+        title="first reason"
+        description="description"
+        image="src/assets/img/hand-0.png"
+      />
     </section>
 
     <section class="w-3/5">Explanation</section>
@@ -9,5 +13,12 @@
 </template>
 
 <script setup lang="ts">
+import { defineProps } from "vue";
 import ReasonsCard from "./ReasonsCard.vue";
+
+const props = defineProps<{
+  title: string;
+  description: string;
+  image: string;
+}>();
 </script>

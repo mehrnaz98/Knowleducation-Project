5f9e882616db116b5134bf4907236a780e1654b8
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 11:28:49 +0330

    Update AppLogo.vue

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,8 +1,13 @@
 <template>
-  <section class="flex justify-start px-7 space-x-2 text-xl">
-    <div>👁</div>
-    <div class="font-bold">Knowleducation</div>
+  <section class="flex justify-start items-center px-7 space-x-2 text-xl">
+    <div class="w-2/5"><BaseAnimatedEye :size="31" /></div>
+
+    <div class="w-3/5">
+      <div class="font-bold">Knowleducation</div>
+    </div>
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseAnimatedEye from "./ui/BaseAnimatedEye.vue";
+</script>

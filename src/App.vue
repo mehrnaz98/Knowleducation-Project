6228a0db6b559941bca06bd512302ce30a8c646f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 19:13:20 +0330

    Import AppCourses component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <AppHeader />
   <AppHero />
   <AppReasons />
+  <AppCourses />
 </template>
 
 <script setup lang="ts">
 import AppHeader from "./components/AppHeader.vue";
 import AppHero from "./components/AppHero.vue";
+import AppCourses from "./components/courses/AppCourses.vue";
 import AppReasons from "./components/reasons/AppReasons.vue";
 </script>

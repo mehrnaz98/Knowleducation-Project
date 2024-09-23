7440339abb62df7c5f61792623c2f4b3f8bee6d1
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 17:34:05 +0330

    Update title and description section

--- src/components/reasons/AppReasons.vue
+++ src/components/reasons/AppReasons.vue
@@ -11,7 +11,22 @@
       </div>
     </section>
 
-    <section class="w-3/5">Explanation</section>
+    <section class="w-3/5 pr-24">
+      <h2 class="text-4xl mt-52 font-bold">
+        Choose Knowleducation over
+        <span>All</span>
+        others!
+      </h2>
+      <p class="text-gray-700 text-xl mt-4 leading-10">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem,
+        commodi, porro nesciunt voluptatum aliquid fuga molestias explicabo
+        pariatur accusantium ullam nobis, consequuntur aliquam laboriosam unde
+        reiciendis doloribus non eveniet iste beatae et mollitia esse voluptas
+        nostrum. Fugiat quasi, rem, nisi error dignissimos architecto animi
+        vitae sunt laboriosam quis voluptatibus, labore quam? Nihil blanditiis
+        ea porro, incidunt magni sed corporis ratione?
+      </p>
+    </section>
   </section>
 </template>
 

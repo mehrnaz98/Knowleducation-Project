9b5f9615a717d73aced53bd64c461b3bdff62c1d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 16:44:59 +0330

    Add transition transform and ease in out to rotate the hand smoother in less than half a second

--- src/components/reasons/ReasonsCard.vue
+++ src/components/reasons/ReasonsCard.vue
@@ -3,12 +3,12 @@
     class="bg-white flex items-center ml-36 w-96 border rounded-lg p-4 shadow-md group"
   >
     <div
-      class="bg-yellow-300 flex justify-center relative border rounded-full w-28 h-28 mx-6 my-5 group-hover:rotate-[-12deg]"
+      class="bg-yellow-300 flex justify-center relative border rounded-full w-28 h-28 mx-6 my-5"
     >
       <img
         :src="image"
         alt="Card Image"
-        class="h-32 object-cover rounded-t-lg absolute bottom-3"
+        class="h-32 object-cover rounded-t-lg absolute bottom-3 transition-transform duration-100 ease-in-out group-hover:rotate-[-12deg]"
       />
     </div>
 

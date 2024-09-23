43f3701bf6c8a5bbabc685ecd282b3b038a42fb2
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 20:47:34 +0330

    Add two sections for the the image and the description

--- src/components/courses/CoursesCard.vue
+++ src/components/courses/CoursesCard.vue
@@ -1,1 +1,18 @@
-<template></template>
+<template>
+  <div
+    class="relative bg-gray-400 flex flex-col w-[25.9rem] h-[33rem] rounded-2xl"
+  >
+    <div
+      class="absolute bg-white w-[23rem] h-[23rem] top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
+    >
+      <p class="uppercase text-6xl font-bold text-gray-400">image</p>
+    </div>
+
+    <div class="mt-[26rem] mx-6 text-lg text-gray-500 flex justify-start">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quis
+        obcaecati accusamus ad perferendis.
+      </p>
+    </div>
+  </div>
+</template>

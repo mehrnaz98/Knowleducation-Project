7a614d109e66d1413d001ab0e0cd401594c6fe87
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 21:51:16 +0330

    Fetch images from the server
    If server response was ok, the images are fetched and the value of the images array will be updated with images or an empty array in case there were no images.

--- src/stores/useCoursesStore.ts
+++ src/stores/useCoursesStore.ts
@@ -4,4 +4,13 @@ import { defineStore } from "pinia";
 export const useCoursesStore = defineStore("courses", () => {
   const images = ref<string[]>([]);
   const isLoading = ref(true);
+
+  const fetchImages = async () => {
+    try {
+       const response = await fetch("https://example.com/api/courses")
+       const data = await response.json();
+    
+     images.value = data.images || [];
+    }
+  }
 });

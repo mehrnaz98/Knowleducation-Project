095fac0d03abd771d6f138a709e8eb4e6c7db082
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 11:43:18 +0330

    Add an event listeners to the <section> element to make the pupil respond to hover whenever the mouse gets close to the section

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,6 +1,10 @@
 <template>
-  <section class="flex justify-start items-center px-7 space-x-2 text-xl">
-    <div class="w-2/5"><BaseAnimatedEye :size="31" /></div>
+  <section
+    class="flex justify-start items-center px-7 space-x-2 text-xl"
+    @mousemove="handleMouseMove"
+    @mouseleave="resetPupil"
+  >
+    <div class="w-2/5"><BaseAnimatedEye :size="31" ref="eyeRef" /></div>
 
     <div class="w-3/5">
       <div class="font-bold">Knowleducation</div>
@@ -10,4 +14,21 @@
 
 <script setup lang="ts">
 import BaseAnimatedEye from "./ui/BaseAnimatedEye.vue";
+import { ref } from "vue";
+
+const eyeRef = ref(null);
+
+const handleMouseMove = (event: MouseEvent) => {
+  if (eyeRef.value) {
+    // Call the movePupil method in the BaseAnimatedEye component
+    eyeRef.value.movePupil(event);
+  }
+};
+
+const resetPupil = () => {
+  if (eyeRef.value) {
+    // Call the resetPupil method in the BaseAnimatedEye component
+    eyeRef.value.resetPupil();
+  }
+};
 </script>

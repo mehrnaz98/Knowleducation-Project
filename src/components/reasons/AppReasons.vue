6386d28d5315fe8ed795adc92c7ed757cb4662f0
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 13:16:24 +0330

    Import ReasonsCard

--- src/components/reasons/AppReasons.vue
+++ src/components/reasons/AppReasons.vue
@@ -1,7 +1,13 @@
 <template>
   <section class="flex">
-    <section class="w-2/5">Cards</section>
+    <section class="w-2/5">
+      <ReasonsCard />
+    </section>
 
     <section class="w-3/5">Explanation</section>
   </section>
 </template>
+
+<script setup lang="ts">
+import ReasonsCard from "./ReasonsCard.vue";
+</script>

e4c524575a7404b3eb80e71abf4321be3f18ba72
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-23 15:10:36 +0330

    Update ReasonsCard.vue

--- src/components/reasons/ReasonsCard.vue
+++ src/components/reasons/ReasonsCard.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="flex w-96 border rounded-lg p-4 shadow-md">
-    <img :src="image" alt="Card Image" class="h-32 object-cover rounded-t-lg" />
-    <div>
-      <h3 class="text-xl font-bold mt-2">{{ title }}</h3>
+  <section
+    class="bg-white flex items-center ml-36 w-96 border rounded-lg p-4 shadow-md"
+  >
+    <div
+      class="bg-yellow-300 flex justify-center relative border rounded-full w-28 h-28 mx-6 my-7"
+    >
+      <img
+        :src="image"
+        alt="Card Image"
+        class="h-32 object-cover rounded-t-lg absolute bottom-3"
+      />
+    </div>
+
+    <div class="leading-10">
+      <h3 class="text-xl font-bold">{{ title }}</h3>
       <p class="text-gray-600">{{ description }}</p>
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">

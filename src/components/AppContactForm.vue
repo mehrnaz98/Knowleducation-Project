d86c395a2a06c5b4fe70ad2404fb8828a5ca2811
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-24 14:17:26 +0330

    Update AppContactForm.vue

--- src/components/AppContactForm.vue
+++ src/components/AppContactForm.vue
@@ -1,15 +1,27 @@
 <template>
-  <h2>Need any help</h2>
-  <form>
-    <label for="fullName">Full Name</label>
-    <input type="text" id="firstName" />
+  <section class="bg-gray-100">
+    <h2>Need any help</h2>
+    <section class="flex bg-white w-full">
+      <section class="w-3/5">
+        <form class="flex flex-col">
+          <div>
+            <label for="fullName">Full Name</label>
+            <input type="text" id="firstName" />
 
-    <label for="email">email</label>
-    <input type="email" id="email" />
+            <label for="email">Email</label>
+            <input type="email" id="email" />
+          </div>
 
-    <label for="message">Message</label>
-    <textarea id="message" row="5"></textarea>
+          <label for="message">Message</label>
+          <textarea id="message" row="5"></textarea>
 
-    <button>Send</button>
-  </form>
+          <button>Send</button>
+        </form>
+      </section>
+
+      <section class="w-2/5">
+        <img src="" alt="" />
+      </section>
+    </section>
+  </section>
 </template>

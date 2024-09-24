784d80bfff225cb376543f0aef66040011b50b60
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-24 13:45:35 +0330

    Update CoursesCard.vue

--- src/components/courses/CoursesCard.vue
+++ src/components/courses/CoursesCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="relative bg-gray-100 flex flex-col w-[25rem] h-[32rem] rounded-2xl"
+    class="relative bg-gray-100 flex flex-col w-[24rem] h-[32rem] rounded-2xl"
   >
     <div
       v-if="image"
-      class="absolute bg-white w-[23rem] h-[23rem] top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
+      class="absolute bg-white w-[22rem] h-[22rem] top-[12rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
     >
       <img
         :src="image"
@@ -14,18 +14,18 @@
     </div>
     <div
       v-else
-      class="absolute bg-white w-[22rem] h-[22rem] top-[12.70rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
+      class="absolute bg-white w-[22rem] h-[22rem] top-[12rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
     >
       <p class="uppercase text-6xl font-bold text-gray-400">image</p>
     </div>
 
     <div
-      class="mt-[24.5rem] mx-6 text-xl text-gray-400 flex justify-start leading-8"
+      class="mt-[23.5rem] mx-6 text-xl text-gray-400 flex justify-start leading-8"
     >
       <p>
         {{
           description ||
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quisobcaecati perferendis."
+          "Lorem ipsum dolor sit amet consecte adipisicing elit. Doloremque quisobcaecati perferendis."
         }}
       </p>
     </div>

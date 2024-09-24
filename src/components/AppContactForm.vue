fbc58c53182fee0085da2a3f214240ba438e385b
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-24 14:06:28 +0330

    Create initial Form

--- src/components/AppContactForm.vue
+++ src/components/AppContactForm.vue
@@ -1,3 +1,15 @@
 <template>
   <h2>Need any help</h2>
+  <form>
+    <label for="fullName">Full Name</label>
+    <input type="text" id="firstName" />
+
+    <label for="email">email</label>
+    <input type="email" id="email" />
+
+    <label for="message">Message</label>
+    <textarea id="message" row="5"></textarea>
+
+    <button>Send</button>
+  </form>
 </template>

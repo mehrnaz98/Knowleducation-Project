93502c6cda0dd08960ff183e7579dc44cd3fcf23
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-24 13:34:51 +0330

    Update SkeletonCard.vue
    Updated width and height. Made three divs representing 3 lines; two of the div have 3 divs inside with display flex representing the divisions between lines.

--- src/components/courses/SkeletonCard.vue
+++ src/components/courses/SkeletonCard.vue
@@ -1,11 +1,24 @@
 <template>
   <div
-    class="relative bg-gray-300 flex flex-col w-[25.9rem] h-[33rem] rounded-2xl animate-pulse"
+    class="relative bg-gray-300 flex flex-col w-[24rem] h-[32rem] rounded-2xl animate-pulse"
   >
     <div
-      class="absolute bg-gray-200 w-[23rem] h-[23rem] top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
-    >
-      <p class="uppercase text-6xl font-bold text-gray-400">Loading...</p>
+      class="absolute bg-gray-200 w-[22rem] h-[22rem] top-[12rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
+    ></div>
+    <div class="mt-[24rem] ml-4 flex flex-col items-start space-y-2">
+      <div class="flex justify-start w-full space-x-2">
+        <div class="bg-gray-200 w-[54%] h-7 rounded-full"></div>
+        <div class="bg-gray-200 w-[24%] h-7 rounded-full"></div>
+        <div class="bg-gray-200 w-[8%] h-7 rounded-full"></div>
+      </div>
+      <div class="flex justify-start w-full space-x-2">
+        <div class="bg-gray-200 w-[24%] h-7 rounded-full"></div>
+        <div class="bg-gray-200 w-[8%] h-7 rounded-full"></div>
+        <div class="bg-gray-200 w-[54%] h-7 rounded-full"></div>
+      </div>
+      <div class="bg-gray-200 w-[54%] h-7 rounded-full"></div>
     </div>
   </div>
 </template>
+
+<style scoped></style>

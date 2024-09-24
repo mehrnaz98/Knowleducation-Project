3b68849002d29a03e586a4594bb529930879afed
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-24 13:49:17 +0330

    Import App contact form

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
   <AppHero />
   <AppReasons />
   <AppCourses />
+  <AppContactForm />
 </template>
 
 <script setup lang="ts">
+import AppContactForm from "./components/AppContactForm.vue";
 import AppHeader from "./components/AppHeader.vue";
 import AppHero from "./components/AppHero.vue";
 import AppCourses from "./components/courses/AppCourses.vue";

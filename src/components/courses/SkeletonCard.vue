9262af2ad6da11f850a3b7b499ae6e9bec5faeac
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-24 12:23:33 +0330

    Create SkeletonCard.vue
    Created skeleton loading screen.

---
+++ src/components/courses/SkeletonCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="relative bg-gray-300 flex flex-col w-[25.9rem] h-[33rem] rounded-2xl animate-pulse"
+  >
+    <div
+      class="absolute bg-gray-200 w-[23rem] h-[23rem] top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl flex justify-center items-center"
+    >
+      <p class="uppercase text-6xl font-bold text-gray-400">Loading...</p>
+    </div>
+  </div>
+</template>

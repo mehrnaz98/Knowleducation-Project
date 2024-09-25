dc6302944310ed70f73b40d4335ff2f225eb9ee1
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-25 23:25:07 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 <script setup lang="ts">
 import AppContactForm from "./components/AppContactForm.vue";
 import AppFooter from "./components/AppFooter.vue";
-import AppHeader from "./components/AppHeader.vue";
+import AppHeader from "./components/layout/AppHeader.vue";
 import AppHero from "./components/AppHero.vue";
 import AppCourses from "./components/courses/AppCourses.vue";
 import AppReasons from "./components/reasons/AppReasons.vue";

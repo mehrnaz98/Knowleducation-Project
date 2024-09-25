bcdac7dbf0256a8cee8e1efa4bcd3d7d45fa537f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-25 22:00:05 +0330

    Create the Hi 5 paragraph

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,6 +4,9 @@
     <div
       class="absolute inset-x-0 top-0 -mt-40 h-40 bg-[#0a0e23] border-t-[15px] border-green-500 rounded-t-[100%_140px] w-full z-10"
     >
+      <div class="pt-20">
+        <p class="text-lg text-gray-300">Give me FIVE 🖐</p>
+      </div>
     </div>
   </footer>
 </template>

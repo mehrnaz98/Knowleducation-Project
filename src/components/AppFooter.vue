ed771d74f9e8ae418acfb3c16eb8be28816271d7
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-25 22:17:22 +0330

    Update AppFooter.vue
    Add three absolute divs inside relative footer, two for the dark blue and green divs and one for the paragraph and the svg. Use z-10 for the dark div to bring it on top of the green div.

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,13 @@
 <template>
   <footer class="relative text-center">
-    <div
-      class="absolute inset-x-0 top-0 -mt-40 h-40 bg-[#0a0e23] border-t-[15px] border-green-500 rounded-t-[100%_140px] w-full z-10"
-    >
-      <div class="pt-20">
+    <div class="relative">
+      <div
+        class="absolute inset-x-0 -mt-40 h-36 bg-green-500 rounded-t-[100%_140px] w-full z-0"
+      ></div>
+      <div
+        class="absolute inset-x-0 -mt-[9.4rem] h-40 bg-[#0a0e23] rounded-t-[100%_140px] w-full z-10"
+      ></div>
+      <div class="absolute inset-x-0 -mt-20 z-10">
         <p class="text-lg text-gray-300">Give me FIVE 🖐</p>
       </div>
     </div>

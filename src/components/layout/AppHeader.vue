74eb1c0c0b850fc193beca2c33ae27866834f516
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-26 18:52:19 +0330

    Decrease header  width

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-gray-100 p-5 pt-16 border-t-8 border-emerald-400">
     <div
-      class="bg-white shadow-lg shadow-gray-200 w-[52rem] h-[5.6rem] flex items-center rounded-full mx-20 py-4"
+      class="bg-white shadow-lg shadow-gray-200 w-[51rem] h-[5.6rem] flex items-center rounded-full mx-20 py-4"
     >
       <section class="w-1/4">
         <AppLogo />

7e4c760f4a250968569820f7ab4a2ffb61461993
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-26 18:53:51 +0330

    Increase button width

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -16,7 +16,7 @@
             <li><a href="#">Contact Us</a></li>
             <li><a href="#">Lorem</a></li>
 
-            <base-button :width="185" :height="55" :font-size="20"
+            <base-button :width="195" :height="55" :font-size="20"
               >Contact Us</base-button
             >
           </ul>

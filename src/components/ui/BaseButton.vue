77189e5cf7814cb7b6bceaedbadf5d1b75233849
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-26 18:39:40 +0330

    Update base button to accept the width, height, and font size as props

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button
+    class="px-6 my-6 bg-emerald-400 text-white rounded-full text-xl"
+    :style="{
+      width: width + 'px',
+      height: height + 'px',
+      fontSize: fontSize + 'px',
+    }"
+  >
+  </button>
+</template>

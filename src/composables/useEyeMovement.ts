28bc234d707665080b11c0c85659328e248e91b3
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-27 22:41:44 +0330

    Create targetX and targetY Variables to store the target position of the pupil when the mouse moves

--- src/composables/useEyeMovement.ts
+++ src/composables/useEyeMovement.ts
@@ -2,4 +2,6 @@ import { ref, onBeforeUnmount } from "vue";
 export function useEyeMovement(size: number, pupilSize: number) {
   const pupilX = ref<number>(0);
   const pupilY = ref<number>(0);
+  let targetX = 0;
+  let targetY = 0;
 }

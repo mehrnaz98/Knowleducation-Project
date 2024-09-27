5139a9171dfb482b6fffcd323f9c573aba5295a3
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-27 15:37:19 +0330

    Add transition all with duration 300 to have a smooth transition

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -40,13 +40,13 @@ const buttonClass = ref("relative left-14");
 const handleScroll = () => {
   if (window.scrollY > 650) {
     headerClass.value =
-      "sticky top-0 z-10 mt-[4.5rem] ml-[6.5rem] bg-white shadow-lg shadow-gray-200 w-[87%] h-[5.6rem] flex items-center rounded-full";
+      "sticky top-0 z-10 mt-[4.5rem] ml-[6.5rem] bg-white shadow-lg shadow-gray-200 w-[87%] h-[5.6rem] flex items-center rounded-full transition-all duration-300";
     rightSectionClass.value =
       "w-3/4 flex justify-center items-center mr-[10rem] text-md-custom font-semibold text-gray-400";
     buttonClass.value = "relative left-[21.5rem]";
   } else {
     headerClass.value =
-      "sticky top-0 z-10 mt-[4.5rem] ml-[6.5rem] bg-white shadow-lg shadow-gray-200 w-[54%] h-[5.6rem] flex items-center justify-center rounded-full";
+      "sticky top-0 z-10 mt-[4.5rem] ml-[6.5rem] bg-white shadow-lg shadow-gray-200 w-[54%] h-[5.6rem] flex items-center justify-center rounded-full transition-all duration-300";
     rightSectionClass.value =
       "w-3/4 flex justify-center items-center text-md-custom font-semibold text-gray-400";
     buttonClass.value = "relative left-14";

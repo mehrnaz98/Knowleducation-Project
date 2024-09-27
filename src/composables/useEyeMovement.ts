fdc0da1e25af2df042dda77a852644a48f06a93d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-27 23:10:05 +0330

    Update useEyeMovement.ts

--- src/composables/useEyeMovement.ts
+++ src/composables/useEyeMovement.ts
@@ -11,9 +11,12 @@ export function useEyeMovement(size: number, pupilSize: number) {
     const { clientX, clientY } = event;
     const target = event.currentTarget as HTMLElement;
     const { left, top, width, height } = target.getBoundingClientRect();
+
     const centerX = left + width / 2;
     const centerY = top + height / 2;
+
     const deltaX = clientX - centerX;
     const deltaY = clientY - centerY;
+
   };
 }

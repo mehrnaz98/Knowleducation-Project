90e617688bf3db3117e1b940ef874b0f84b894d5
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-28 11:27:27 +0330

    Update ReasonsCard.vue

--- src/components/reasons/ReasonsCard.vue
+++ src/components/reasons/ReasonsCard.vue
@@ -13,12 +13,12 @@
     </div>
 
     <div class="leading-10">
-      <h3 class="font-righteous text-xl text-slate-600">{{ title }}</h3>
-      <p class="text-gray-400">{{ description }}</p>
+      <h3 class="font-righteous text-[1.4rem] text-slate-600">{{ title }}</h3>
+      <p class="text-gray-400 text-[1.1rem]">{{ description }}</p>
     </div>
 
     <div
-      class="absolute transform rotate-45 z-0 opacity-20 right-0 pr-4 pt-0 -mt-10"
+      class="absolute transform rotate-45 z-0 opacity-15 right-0 pr-4 pt-0 -mt-10"
     >
       <p class="font-righteous text-6xl font-semibold text-gray-400 text-right">
         {{ ordinalNumber }}
